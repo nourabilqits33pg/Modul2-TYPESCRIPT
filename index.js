c763df2be77eb7b2e246d13b6d646ab0f56b7c40
nourabilqits33pg <noura_bilqits_33pg@student.smktelkom-mlg.sch.id>
2026-04-15 08:25:21 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+// CONTOH SOAL
 var nama = "Budi";
 var umur = 17;
 var aktif = true;
 console.log(nama, umur, aktif);
+var student1 = {
+    id: 7,
+    studentName: "Noura",
+    studentClass: "11",
+    birthYear: 2009
+};
+console.log(student1);
+//TUGAS 2 : FUNCTION HITUNG UMUR
+function AgeCount(birthYear) {
+    return 2026 - birthYear;
+}
+console.log(student1.studentName, AgeCount(student1.birthYear), student1.studentClass);
+//QnA
+// 1. Perbedaan utama JavaScript dan TypeScript menurut Anda adalah : perbedaan variable dan struktur ngoding nya
+// 2. Bagian yang paling membantu Anda memahami TypeScript adalah : modul,bantuan pak ilham, bantuan teman, dan bantuan AI
+// 3. Bagian yang masih membingungkan dan perlu penguatan adalah : masih belum menguasai secara betul pengguanaan typescript
